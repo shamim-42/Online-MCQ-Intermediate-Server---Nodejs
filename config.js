df7d44aa03f215936fd8038cdd86fa1dc9451fbb
shamim42 <shamim.cse.bd@gmail.com>
2020-10-18 12:50:39 +0600

    config chanded

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "http://3.17.29.219:8000"
-const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "http://3.17.29.219:8000"
+// const BASE_URL = "http://localhost:8000"
 const JWT_SECRET_KEY="KSJFO2UI02xlkdfj2oq3ru298wk"
